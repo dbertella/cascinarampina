535f726a2e317ba6c4e343ea24639b2c3895d874
Daniele Bertella <daniele.bertella@gmail.com>
2020-11-08 22:37:13 +0000

    Fix body class, and clean up

--- components/header.tsx
+++ components/header.tsx
@@ -1,31 +1,30 @@
 import Link from "next/link";
-import { Box, Flex, Image, NavLink, Button } from "theme-ui";
+import { Box, Flex, Image, NavLink, Button, MenuButton } from "theme-ui";
 
 import React, { useState } from "react";
 import MEDIA_QUERY from "../constants/mq.js";
 
-
 export default function Header() {
   const [isActive, setActive] = useState(false);
   const toggleMenu = () => {
-    const bodyClasses = document && document.body.classList
-    setActive(!isActive)
-
-    isActive ? bodyClasses.remove('overflow-hidden') : bodyClasses.add('overflow-hidden');
-  }
+    document?.body?.classList?.toggle?.("overflow-hidden");
+    setActive(!isActive);
+  };
 
   const mobileLinksStyle = {
     [MEDIA_QUERY.mobile]: {
-      fontSize: 18, 
-      margin: 2
-    }
-  }
+      fontSize: 18,
+      margin: 2,
+    },
+  };
 
   return (
-    <Box
+    <Flex
       sx={{
-        position: 'relative',
         boxShadow: (t) => `0 0 25px ${t.colors.primaryTransparent}`,
+        flexFlow: ["row", null, "column"],
+        justifyContent: "space-between",
+        alignItems: "center",
       }}
     >
       <Flex sx={{ justifyContent: "center" }}>
@@ -40,74 +39,75 @@ export default function Header() {
       </Flex>
       <Flex
         as="nav"
-        className={isActive ? "d-flex" : ''}
         sx={{
           justifyContent: "center",
           [MEDIA_QUERY.mobile]: {
-            display: 'none',
-            position: 'fixed',
+            display: isActive ? "flex" : "none",
+            position: "fixed",
             left: 0,
             right: 0,
             bottom: 0,
-            bg: 'background',
+            bg: "background",
             zIndex: 100,
             top: 100,
-            flexDirection: 'column',
-            alignItems: 'center',
-          } 
-        }}>
+            flexDirection: "column",
+            alignItems: "center",
+          },
+        }}
+      >
         <Link href="/" passHref>
-          <NavLink sx={mobileLinksStyle} p={2}>Home</NavLink>
+          <NavLink sx={mobileLinksStyle} p={2}>
+            Home
+          </NavLink>
         </Link>
         <Link href="/dove-siamo" passHref>
-          <NavLink sx={mobileLinksStyle} p={2}>Dove Siamo</NavLink>
+          <NavLink sx={mobileLinksStyle} p={2}>
+            Dove Siamo
+          </NavLink>
         </Link>
         <Link href="/chi-siamo" passHref>
-          <NavLink sx={mobileLinksStyle} p={2}>Chi Siamo</NavLink>
+          <NavLink sx={mobileLinksStyle} p={2}>
+            Chi Siamo
+          </NavLink>
         </Link>
         <Link href="/cosa-facciamo" passHref>
-          <NavLink sx={mobileLinksStyle} p={2}>Cosa Facciamo</NavLink>
+          <NavLink sx={mobileLinksStyle} p={2}>
+            Cosa Facciamo
+          </NavLink>
         </Link>
         <Link href="/contatti" passHref>
-          <NavLink sx={mobileLinksStyle} p={2}>Contatti</NavLink>
+          <NavLink sx={mobileLinksStyle} p={2}>
+            Contatti
+          </NavLink>
         </Link>
         <Link href="/categorie-prodotti" passHref>
-          <NavLink sx={mobileLinksStyle} p={2}>Prodotti</NavLink>
+          <NavLink sx={mobileLinksStyle} p={2}>
+            Prodotti
+          </NavLink>
         </Link>
         <Link href="/faq" passHref>
-          <NavLink sx={mobileLinksStyle} p={2}>FAQs </NavLink>
+          <NavLink sx={mobileLinksStyle} p={2}>
+            FAQs{" "}
+          </NavLink>
         </Link>
 
         <Link href="/news" passHref>
-          <NavLink sx={mobileLinksStyle} p={2}>News</NavLink>
+          <NavLink sx={mobileLinksStyle} p={2}>
+            News
+          </NavLink>
         </Link>
       </Flex>
-      <Button
-        className="burger-menu"
+      <MenuButton
+        aria-label="Toggle Menu"
         onClick={toggleMenu}
         sx={{
-          position: 'absolute',
-          cursor: 'pointer',
-          right: 20,
-          top: 20,
-          appearance: 'none',
-          display: 'inline-block',
-          textAlign: 'center',
-          lineHeight: 'inherit',
-          textDecoration: 'none',
-          p: 2,
-          backgroundColor: t => t.colors.mutedTransparent,
-          ':hover': {
-            backgroundColor: t => t.colors.mutedTransparent,
+          display: ["block", null, "none"],
+          backgroundColor: "mutedTransparent",
+          ":hover": {
+            backgroundColor: "mutedTransparent",
           },
-          [MEDIA_QUERY.d('min', 768)]: {
-            display: 'none'
-          }
-        }}>
-        <span className="line"></span>
-        <span className="line"></span>
-        <span className="line"></span>
-      </Button>
-    </Box>
+        }}
+      />
+    </Flex>
   );
 }
