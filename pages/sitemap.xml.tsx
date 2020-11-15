efa8e9bfd67af58d4620571d6cfa1e1891fcabac
Daniele Bertella <daniele.bertella@gmail.com>
2020-11-15 23:32:25 +0000

    Add static list of pages to fix sitemap for now

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,17 +1,29 @@
 import { GetServerSideProps } from "next";
-import globby from "globby";
 import {
   getAllPostsWithSlug,
   getAllProductCategoriesWithSlug,
   getAllProductsWithSlug,
 } from "lib";
+// import globby from "globby"; // this doens't work in prod, leaving it here if I want to use it again in the future
+// const pages = await globby([
+//   "pages/**/*.tsx",
+//   "!pages/api",
+//   "!pages/**/\\[*\\].tsx",
+//   "!pages/**/*.xml.tsx",
+// ]).then((all) =>
+//   all.map((route) =>
+//     route.replace("pages", "").replace(".tsx", "").replace("/index", "")
+//   )
+// );
 
 const createSitemap = (
   routes: string[],
   host?: string
 ) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${routes.map((route) => `<url><loc>https://${host}${route}</loc></url>`).join("")}
+    ${routes
+      .map((route) => `<url><loc>https://${host}${route}</loc></url>`)
+      .join("")}
     </urlset>`;
 
 const Sitemap = () => {};
@@ -25,16 +37,25 @@ type Post = {
 };
 
 export const getServerSideProps: GetServerSideProps = async ({ res, req }) => {
-  const pages = await globby([
-    "pages/**/*.tsx",
-    "!pages/api",
-    "!pages/**/\\[*\\].tsx",
-    "!pages/**/*.xml.tsx",
-  ]).then((all) =>
-    all.map((route) =>
-      route.replace("pages", "").replace(".tsx", "").replace("/index", "")
-    )
-  );
+  const pages = [
+    "",
+    "/chi-siamo",
+    "/contatti",
+    "/cosa-facciamo",
+    "/dove-siamo",
+    "/faq",
+    "/regali-natale-2020",
+    "/categorie-prodotti",
+    "/cosa-facciamo/alleviamo",
+    "/cosa-facciamo/coltiviamo",
+    "/cosa-facciamo/condividiamo",
+    "/cosa-facciamo/cresciamo",
+    "/cosa-facciamo/raccogliamo",
+    "/cosa-facciamo/trasformiamo",
+    "/cosa-facciamo/vendiamo",
+    "/news",
+    "/prodotti",
+  ];
   const news = await getAllPostsWithSlug().then((all) =>
     all.edges.map(({ node }: Post) => `/news/${node.slug}`)
   );
